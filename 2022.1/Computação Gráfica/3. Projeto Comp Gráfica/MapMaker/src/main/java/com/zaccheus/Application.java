c951e60c595050ad6225a2185b23f975cb408935
Daniel Muller <dani.a.muller@hotmail.com>
2022-05-04 22:47:06 -0300

    projeto comp grafica

--- 2022.1/Computação Gráfica/3. Projeto Comp Gráfica/MapMaker/src/main/java/com/zaccheus/Application.java
+++ 2022.1/Computação Gráfica/3. Projeto Comp Gráfica/MapMaker/src/main/java/com/zaccheus/Application.java
@@ -8,7 +8,6 @@ import com.zaccheus.model.map.writer.WaveWriter;
 
 public class Application{
     public static void main(String[] args){
-
         //The console these are printing out to may need to be resized to fit the complete
         // length of the wave and map on the screen.
 
@@ -16,21 +15,26 @@ public class Application{
         //WaveGenerator waveGen = new WaveGenerator(2.1, 0.8, 4, 40);
         //WaveWriter.printWaveToConsoleHorizontally(waveGen.generateWave());
 
-        //Generate and save to the image "sample_map.png" a map with default inputs
-        MapGenerator mapGen = new MapGenerator();
-        MapWriter.generateColorImage(mapGen.generateMap(false), "sample_map");
+        long start = System.currentTimeMillis();
+        for (int i = 0; i < 1; i++){
+            //Generate and save to the image "sample_map.png" a map with default inputs
+            MapGenerator mapGen = new MapGenerator();
+            MapWriter.generateColorImage(mapGen.generateMap(false), "sample_map");
 
-        //Generate and save to the image "sample_island_map.png" an island map with default inputs
-        MapWriter.generateColorImage(mapGen.generateMap(true), "sample_island_map");
+            //Generate and save to the image "sample_island_map.png" an island map with default inputs
+            MapWriter.generateColorImage(mapGen.generateMap(true), "sample_island_map");
 
-        //Generate and print to console an ASCII art map using custom inputs
-        System.out.println("\n\n\n\n"); //Space this out after the wave is printed
-        MapGenerator asciiGen = new MapGenerator(2.9, 0.4, 4, 40, 50, 150);
-        MapWriter.generateASCII(asciiGen.generateMap(false));
+            //Generate and print to console an ASCII art map using custom inputs
+            //System.out.println("\n\n\n\n"); //Space this out after the wave is printed
+            //MapGenerator asciiGen = new MapGenerator(2.9, 0.4, 4, 40, 50, 150);
+            //MapWriter.generateASCII(asciiGen.generateMap(false));
 
-        //Generate and save to the image TileMap.png a map using tiles
-        MapCombiner combiner = new MapCombiner(700, 1400, true);
-        MapWriter.generateTileImage(combiner.generateTileMap());
+            //Generate and save to the image TileMap.png a map using tiles
+            MapCombiner combiner = new MapCombiner(700, 1400, true);
+            MapWriter.generateTileImage(combiner.generateTileMap());
+        }
+        long elapsed = System.currentTimeMillis() - start;
 
+        System.out.println("Executed in: " + elapsed + "ms");
     }
 }
